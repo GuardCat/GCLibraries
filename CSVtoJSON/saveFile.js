1c78597da66a4f787a11ac73d4f5be948ce2c6a4
storktec@mail.ru <storktec@mail.ru>
2019-04-07 00:21:27 +0300

    Add csv to json draft

---
+++ CSVtoJSON/saveFile.js
@@ -0,0 +1,8 @@
+function saveFile(content, settings={name:"untitled.txt", type: "text/json", charset: "utf=8"}) {
+		let link = document.createElement("a");
+		link.style.display="none";
+		link.setAttribute("download", settings.name)
+		link.setAttribute("href", `data:${settings.type};charset=${settings.charset},${encodeURI(content)}`);
+		document.body.appendChild(link);
+		link.click();
+}
