088f82df65290cc8773dac3da8bfc652a9975077
storktec@mail.ru <storktec@mail.ru>
2019-09-18 19:42:26 +0300

    Начали функцию сжатия массивов объектов

---
+++ compressJSON.js
@@ -0,0 +1,37 @@
+const tinyJSON = {
+	compress: (arr) => {
+		return arr.reduce( (result, item) => {
+			for (let key in item) {
+				if (!item.hasOwnProperty(key)) continue;
+				if (!result[key]) result[key] = [ ];
+				result[key].push(item[key]);
+			}
+			return result
+		}, { })
+	},
+
+	deflate: (obj) => {
+		let result = [ ], entry;
+		for (let key in obj) {
+			entry = { };
+			if (!obj.hasOwnProperty(key)) continue;
+			entry
+		}
+		return arr.reduce( (result, key) => {
+
+		}, { })
+	}
+};
+
+const a = [
+	{a: 1, b: 2},
+	{a: 1, b: 2},
+	{a: 1, b: 2}
+]
+
+const b = tinyJSON.compress(a);
+
+gc.log( encodeURIComponent(JSON.stringify(a)).length);
+gc.dir(b);
+gc.info( encodeURIComponent(JSON.stringify(b)).length);
+
