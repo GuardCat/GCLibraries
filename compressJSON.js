8e1ddca09e8c5b10321e2aab11c919a18b819452
storktec@mail.ru <storktec@mail.ru>
2019-09-18 23:56:36 +0300

    Функция сжатия массивов объектов Refactoring

--- compressJSON.js
+++ compressJSON.js
@@ -15,19 +15,19 @@ const tinyJSON = {
 			keys = Object.keys(obj),
 			mainKey = keys.reduce( (res, key) => obj[key].length > res.len ? {key: key, len: obj[key].length} : res, {len: 0} ).key
 		;
-		return obj[mainKey].map( (item, i) => keys.reduce( (res, key) => { res[key] = obj[key][i]; return res }, { }), [ ]);
+		return obj[mainKey].map( (item, i) => keys.reduce((res, key) => { res[key] = obj[key][i]; return res }, { }) );
 	}
 };
 
 const a = [
-	{a: 1, b: 2},
-	{a: 1, b: 2},
-	{a: 1, b: 2}
+	{ar: 10, bw: 2},
+	{ar: 109, bw: 250},
+	{ar: 1987, bw: 2000}
 ]
 
 const b = tinyJSON.compress(a);
-gc.log( JSON.stringify(tinyJSON.deflate(b), false, 2) );/*
+gc.log( JSON.stringify(tinyJSON.deflate(b), false, 2) );
 
 gc.log( encodeURIComponent(JSON.stringify(a)).length);
 gc.dir(b);
-gc.info( encodeURIComponent(JSON.stringify(b)).length);*/
+gc.info( encodeURIComponent(JSON.stringify(b)).length);
