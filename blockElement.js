6a6de364c43cc3c01a25dee172b0cb1087d5dda2
storktec@mail.ru <storktec@mail.ru>
2019-11-14 08:11:09 +0300

    Рефакторинг и докумениирование Observer

--- blockElement.js
+++ blockElement.js
@@ -1,5 +1,5 @@
-/*jshint es5: true, esnext: true, loopfunc: true, browser: true, devel: true*/
-const 
+/*jshint esversion: 5, esnext: true, loopfunc: true, browser: true, devel: true*/
+const
 	keySettings = {
 		ups:		{min: 2, max: 5},
 		lows:		{min: 2, max: 4},
@@ -14,7 +14,7 @@ function genSeq(min, max) {
 		if ( !(i && i % 2) ) res.push( genSeq(el, arr[i + 1]) );
 		return res.flat( );
 	}, [ ]);
-	
+
 	let result = [ ];
 	for (let i = min; i < max; i++) {
 		result.push( String.fromCharCode(i) );
@@ -29,18 +29,18 @@ function genKey(sets) {
 		lows:	genSeq(97, 122),
 		nums:	genSeq(48, 57)
 	};
-	let result = "", pieceLen = 0; 
-	
+	let result = "", pieceLen = 0;
+
 	while (result.length < sets.len) {
 		for (let i in letters) {
 			if ( !letters.hasOwnProperty(i) ) continue;
 			pieceLen = rnd(sets[i].min, sets[i].max);
 			if (result.length + pieceLen > sets.len) pieceLen -= result.length + pieceLen - sets.len;
-				
-			result += getRndEls(letters[i], pieceLen).join("");		
+
+			result += getRndEls(letters[i], pieceLen).join("");
 		}
 	}
-	
+
 	return shakeText(result);
 }
 
@@ -50,7 +50,7 @@ function shakeText(text) {
 	return text.join("");
 }
 
-function rnd(min, max) { 
+function rnd(min, max) {
     return +( Math.random( ) * (max - min) + min ).toFixed(0);
 }
 
@@ -68,17 +68,16 @@ function getRndEls(arr, len) {
 
 
 function director(el) {
-	const 
+	const
 		key = genKey(keySettings),
 		message = `
 		<div  id = "dialog">
-			<H2>Отказ от предоставления e-mail</H2>
-			На свой E-mail клиент сможет получать напоминания о платежах или запрошенные документы.</div>
-			<div>Если у клиента есть почтовый адрес, нажмите «Отмена» и введите почту клиента</div>
-			<div>Иначе введите код отказа, указанный ниже и нажмите „Ok” В коде различаются строчные и прописные буквы, будьте внимательны.</div>
+			<H2>ÐÑÐºÐ°Ð· Ð¾Ñ Ð¿ÑÐµÐ´Ð¾ÑÑÐ°Ð²Ð»ÐµÐ½Ð¸Ñ e-mail</H2>
+			ÐÐ° ÑÐ²Ð¾Ð¹ E-mail ÐºÐ»Ð¸ÐµÐ½Ñ ÑÐ¼Ð¾Ð¶ÐµÑ Ð¿Ð¾Ð»ÑÑÐ°ÑÑ Ð½Ð°Ð¿Ð¾Ð¼Ð¸Ð½Ð°Ð½Ð¸Ñ Ð¾ Ð¿Ð»Ð°ÑÐµÐ¶Ð°Ñ Ð¸Ð»Ð¸ Ð·Ð°Ð¿ÑÐ¾ÑÐµÐ½Ð½ÑÐµ Ð´Ð¾ÐºÑÐ¼ÐµÐ½ÑÑ.</div>
+			<div>ÐÑÐ»Ð¸ Ñ ÐºÐ»Ð¸ÐµÐ½ÑÐ° ÐµÑÑÑ Ð¿Ð¾ÑÑÐ¾Ð²ÑÐ¹ Ð°Ð´ÑÐµÑ, Ð½Ð°Ð¶Ð¼Ð¸ÑÐµ Â«ÐÑÐ¼ÐµÐ½Ð°Â» Ð¸ Ð²Ð²ÐµÐ´Ð¸ÑÐµ Ð¿Ð¾ÑÑÑ ÐºÐ»Ð¸ÐµÐ½ÑÐ°</div>
+			<div>ÐÐ½Ð°ÑÐµ Ð²Ð²ÐµÐ´Ð¸ÑÐµ ÐºÐ¾Ð´ Ð¾ÑÐºÐ°Ð·Ð°, ÑÐºÐ°Ð·Ð°Ð½Ð½ÑÐ¹ Ð½Ð¸Ð¶Ðµ Ð¸ Ð½Ð°Ð¶Ð¼Ð¸ÑÐµ âOkâ Ð ÐºÐ¾Ð´Ðµ ÑÐ°Ð·Ð»Ð¸ÑÐ°ÑÑÑÑ ÑÑÑÐ¾ÑÐ½ÑÐµ Ð¸ Ð¿ÑÐ¾Ð¿Ð¸ÑÐ½ÑÐµ Ð±ÑÐºÐ²Ñ, Ð±ÑÐ´ÑÑÐµ Ð²Ð½Ð¸Ð¼Ð°ÑÐµÐ»ÑÐ½Ñ.</div>
 			<strong>${key}</strong>
 			<input type = "text" id = "noEmailCode">
 		</div>`
 	;
 }
-
--- observer.js
+++ observer.js
@@ -13,42 +13,49 @@ class EventList {
 		if ( !(event in this.list) ) this.list[event] = [ ];
 		if ( !(handler instanceof Function) ) throw new Error("You're trying to add to EventList not Function.");
 		this.list[event].push(handler);
-		return true;
+		return this.del.bind(this, event, handler);
 	}
-  
+
 	del(event, handler) {
 		if ( !(event in this.list) || !this.list[event].length ) return false;
 		this.list[event].forEach( (el, i) => { if (el === handler) return this.list[event].splice(i, 1); } );
-		return false;  
+		return false;
 	}
-  
+
 	drop(event) {
 		if ( !(event in this.list) ) return false;
 		return delete this.list[event];
 	}
-	  
+
 	run(event, e = {}, ...args) {
 		if ( !(event in this.list) ) return false;
 		e.type = event;
-		this.list[event].forEach( (ev) => ev(e, ...args) ); 
+		this.list[event].forEach( (handler) => handler(e, ...args) );
 	}
-	  
+
 }
 
+/**
+ * @desc паттерн «Pub/Sub». Для каждого наблюдаемого объекта —
+ *		свой экземпляр. Устанавливает метод publish наблюдаемому объекту.
+ * @constructor
+ * @param {object} obj наблюдаемый объект. Должен уметь использовать publish.
+ * @require {object} EventList class
+*/
 class Observer {
 	constructor(obj) {
 		this.list = new EventList();
 		obj.publish = this.publish.bind(this);
 	}
-	
+
 	subscribe(event, fn) {
 		return this.list.add(event, fn);
 	}
-	
+
 	unsubscribe(event, fn) {
 		return fn ? this.list.del(event, fn) : this.list.drop(event);
 	}
-	
+
 	publish(event, e, ...args) {
 		return this.list.run(event, e, ...args) ;
 	}
@@ -57,23 +64,34 @@ class Observer {
 /* Тестирование
 class Cat {
 	constructor(name, age) {
-		(this.name = name), (this.age = age);
+		this.name = name;
+		this.age = age;
+		this.publish = ( ) => false;
 	}
-	meow() {
+	meow( ) {
 		console.error("Meeeeooowwww");
-		if (this.publish) this.publish("meowed");
+		this.publish("meowed", {target: this});
 	}
-	whoayou() {
-		console.info(`My name: ${this.name}, my age: ${this.age} moths.`);
+	whoayou( ) {
+		console.info(`My name: ${this.name}, my age: ${this.age} months.`);
+		this.publish("aged", {target: this});
 	}
 }
 
 function itMeowed(e) {
-	console.log(`${this.name} мяукнул`);
+	console.log(`${e.target.name} мяукнул`);
 }
 
+function itAged(e) {
+	console.log(`${e.target.name} спросили о возрасте`);
+}
 var cat = new Cat("Васька", 3);
 var catObserver = new Observer(cat);
-catObserver.subscribe("meowed", itMeowed.bind(cat));
-cat.meow();
-cat.whoayou();*/
+var mewUnsubscribe = catObserver.subscribe("meowed", itMeowed);
+var ageUnsubscribe = catObserver.subscribe("aged", itAged);
+cat.meow( );
+cat.whoayou( );
+
+mewUnsubscribe( );
+cat.meow( );
+*/
