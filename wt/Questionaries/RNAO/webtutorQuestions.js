2c13978324bc7e0c25ee604cb17483ad11ec71f0
GuardCat <storktec@mail.ru>
2019-11-14 13:22:03 +0300

    WT: Опрос РНАО переделали

--- wt/Questionaries/RNAO/webtutorQuestions.js
+++ wt/Questionaries/RNAO/webtutorQuestions.js
@@ -1,70 +1,54 @@
 /*jshint esversion: 6, browser: true, devel: true*/
-( function( ) {
-
-	const
-		questions = Object.assign( [ ], document.querySelectorAll("div[wtp-part='body'] .wtp-question") ),
-		noTrainingsTxt = "В течение последнего месяца тренинги не проводились",
-		anotherTxt = "Другое обучение / тренинг (напишу в комментариях)",
-		hideOnStart = [3, 4],
-		hideBase = { }
+( function ( ) {
+	const 
+		questions = document.querySelectorAll("div[wtp-part='body'] .wtp-question"),
+		questionItemHideAll = 13,
+		groups = [
+			[4, 5], [6, 7]
+		],
+		forHide = [1, 2, 3]
 	;
-
-	let i;
-	hideBase[noTrainingsTxt] = [ 1, 2, 5, 6 ];
-
-	function director( ) {
-		hideOnStart.forEach( el => hide(questions[el]) );
-		questions[0].addEventListener("click", configureQuestionare, false);
-		hideUnHide( );
+	
+	let allHidedFlag = false;
+	window.q = questions;
+	
+	function getChecked(question) {
+		return Object.assign( [ ], question.querySelectorAll("div[wt-state='selected']") );
 	}
 	
-	function getCheckedText(question) {
-		const answers = Object.assign( [ ], question.querySelectorAll(".wt-check-item[wt-state='selected']") );
-		return answers.map( q => q.querySelector("td.wt-check-item-text-container").innerHTML );
-	}
-	
-	function getCheckedOptions(question) {
-		return Object.assign( [ ], question.querySelectorAll(".wt-check-item[wt-state='selected']") );
-	}
-	
-	function configureQuestionare(e) {
-		hideUnHide( );		
-	}
-
-	function hide(el, c = "hided") {
-		if ( !el.classList.contains(c) ) el.classList.add(c);
-	}
-	function unHide(el, c = "hided") {
-		el.classList.remove(c);
-	}
-
-	function hideUnHide( ) {
-		const 
-			textArr = getCheckedText(questions[0]),
-			correction = textArr.some( el => el === noTrainingsTxt ) ? 1 : 0,
-			themesAmount = textArr.length - correction,
-			checked = getCheckedOptions(questions[0]).slice(0, -(themesAmount))
-		;
-		let question;
-
-		hideOnStart.forEach( (el, i) => {
-			if ( (i + correction) < themesAmount) {
-				unHide(questions[el]);
-				question = questions[el].querySelector("div[wtp-role='q-text']");
-				question.innerHTML = question.innerHTML.slice(0, 15) + textArr[i];
-			} else {
-				hide(questions[el]);
-			}
+	function hideQuestions(arr) {
+		arr.forEach( i => { 
+			if( !questions[i].classList.contains("hided") ) questions[i].classList.add("hided"); 
 		} );
-		
-		if ( textArr.some( i => i === noTrainingsTxt ) ) {
-			checked.forEach( el => el.click( ) );
-			hideBase[noTrainingsTxt].forEach( i => hide( questions[i] ) );
-		} else {
-			hideBase[noTrainingsTxt].forEach( i => unHide( questions[i] ) );
-		}
-		
+	}
+	function showQuestions(arr) {
+		arr.forEach( i => {
+			if( questions[i].classList.contains("hided") ) questions[i].classList.remove("hided"); 
+		} );
 	}
 	
-	director( );
-} )( );
+	function configureQuestions(e) {
+		const
+			checked = getChecked(questions[0]),
+			noTrainings = checked.some( el => el.getAttribute("wt-item") === "13" ),
+			checkedTrainingsAmount = noTrainings ? 0 : checked.length
+		;
+		
+		let allHidedFlags = false;
+		
+		if (checkedTrainingsAmount) groups.slice(0, checkedTrainingsAmount).forEach( group => showQuestions(group) );
+		if (checkedTrainingsAmount < groups.length ) groups.slice( -(groups.length - checkedTrainingsAmount) ).forEach( group => hideQuestions(group) );
+		
+		if (noTrainings) {
+			allHidedFlag = true;
+			hideQuestions(forHide);
+			checked.forEach( el => el.getAttribute("wt-item") !== "13" ? el.click( ) : true );
+		} else if (allHidedFlag) {
+			allHidedFlag = false;
+			showQuestions(forHide);
+		}
+	}
+	
+	questions[0].querySelector(".wtp-question-right").addEventListener("click", (e) => setTimeout( configureQuestions.bind(false, e), 10 ), false);
+	configureQuestions( );
+} ) ( );
