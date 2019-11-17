f6591ce0ca7253c8ab5d9a49f3e5b4002db7a189
storktec@mail.ru <storktec@mail.ru>
2019-11-17 23:51:23 +0300

    Завершили элемент GCInput

--- components/forms/simpleinput/simpleinput.js
+++ components/forms/simpleinput/simpleinput.js
@@ -5,16 +5,29 @@ class GCInput {
 		this.titleText = this.title.innerText;
 		this.input.setAttribute("placeholder", this.titleText);
 
-		this.input.addEventListener( "focus", this.title.classList.add("GCInput__title_needed"), false );
-		this.input.addEventListener("blur", this._showHideTitle, false);
+		this.input.addEventListener( "focus", this.showTitle.bind(this), false );
+		this.input.addEventListener( "blur", this._showHideTitle.bind(this), false );
+
+		this._showHideTitle( );
 	}
+
 	_showHideTitle( ) {
-		if (this.value.length) return this.title.classList.add("GCInput__title_needed");
+		if (!this.value.length) return this.hideTitle( );
+		return this.showTitle( );
+	}
+
+	hideTitle( ) {
 		this.title.classList.remove("GCInput__title_needed");
 	}
+
+	showTitle( ) {
+		this.title.classList.add("GCInput__title_needed");
+	}
+
 	get value( ) {
 		return this.input.value;
 	}
+
 	publish( ) {
 		return;
 	}
