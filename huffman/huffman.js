ccfddc71b74ab8b95002239ac73a0ac4a4d359a7
GuardCat <storktec@mail.ru>
2019-11-21 15:01:30 +0300

    Huffman: формируется кодовая таблица

--- huffman/huffman.js
+++ huffman/huffman.js
@@ -1,4 +1,14 @@
+/*jshint es5: true, esnext: true, browser: true*/
 const huffman = {
+	
+	compress(text) {
+		const
+			stat = this.getSymbolsStat(text),
+			pyramid = this.makePyramide( this.makeSortedArray(stat) ),
+			table = this.makeCodeTable(pyramid)
+		;
+		return {table, pyramid};
+	},
 	/**
 	* @desc считает сколько раз каждый символ встречается в тексте
 	* @param {string} text
@@ -15,18 +25,45 @@ const huffman = {
 		}, { }) ;
 	},
 
+	/**
+	 * @desc создает отсортированный массив символов из частотной таблицы
+	 * @param {object} частотный хэш-массив
+	 * @return {array} массив символов, отсортированный по частоте появления
+	 */
 	makeSortedArray(obj) {
 		const symbols = Object.keys(obj);
 		symbols.sort( (a,b) => {
-			if (obj[a] > obj[b]) return 1;
-			if (obj[a] < obj[b]) return -1;
+			if (obj[a] < obj[b]) return 1;
+			if (obj[a] > obj[b]) return -1;
 			return 0;
 		} );
 		return symbols;
+	},
+	
+	/**
+	 * @desc создает бинарное дерево из отсортированного массива символов
+	 */
+	makePyramide(arr) {
+		while (arr.length > 1) {
+			arr.push( arr.splice(-2, 2) );
+		}
+		return arr.flat( );
+	},
+	
+	makeCodeTable(arr, code = "", result = { }) {
+		arr.forEach( (el, i) => {
+			if ( !(el instanceof Array) ) {
+				result[el] = code + i;
+			} else {
+				return this.makeCodeTable(el, code + i, result);
+			}
+		} );
+		return result;
 	}
-}
+	
+};
 /* todo
-функция, из объекта частот создающая отсортированный массив символов
++функция, из объекта частот создающая отсортированный массив символов
 функция сбора пирамидки
 функция создания кодов из пирамидки
 функция кодирования текста
