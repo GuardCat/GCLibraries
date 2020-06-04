7381eac61eefaa201463119cf9d1501763eae70e
GuardCat <storktec@mail.ru>
2020-06-04 14:47:37 +0300

    Написана асинхронная функция загрузки данных. Написана функция отображения данных.

--- okvedSearcher/logic.js
+++ okvedSearcher/logic.js
@@ -1,42 +1,34 @@
 /*jshint esversion: 8, browser: true*/
 
 
-let 
-	GCLocalBase,
-	main
-;
-
 async function director( ) {
 	const 
 		okveds = await getJson("okved.json"),
-		areas =  await getJson("area.json")
+		areas =  await getJson("area.json"),
+		main = document.body.querySelector("main")
 	;
-	main = document.body.querySelector("main");
 	
-	addOkved(okveds[2], main);
+	addRow(okveds[2], areas[2], main);
 }
 
 async function getJson(url) {
 	if (!url) throw new Error(`getJson: recieved empty url`);
 	
-	fetch(url).then( (responce) => {
-			if (!responce.ok) throw new Error(`getJson: Can't recieve the file "${url}". Server response status: ${responce.status}: ${responce.statusText}`);
-			resolve( responce.json( ) );
-	} )	
-	
+	return new Promise( async (resolve, reject) => {
+		let responce = await fetch(url);
+		if (!responce.ok) throw new Error(`getJson: Can't recieve the file "${url}". Server response status: ${responce.status}: ${responce.statusText}`);
+		resolve( await responce.json( ) );
+	} );
 }
 
-function addOkved(entry, block) {
-	block.insertAdjacentHTML( "beforeend", createO(entry) );
-}
-
-function addArea(entry, block) {
-	block.insertAdjacentHTML( "beforeend", createA(entry) );
+function addRow(okvedEntry, areaEntry, block) {
+	block.insertAdjacentHTML( "beforeend", createO(okvedEntry) );
+	block.insertAdjacentHTML( "beforeend", createA(areaEntry) );
 }
 
 
-function createO(entry) {
-	return `<p>${entry.name}</p>`;
+function createA(entry) {
+	return `<p>${entry.area}</p>`;
 }
 
 function createO(entry) {
