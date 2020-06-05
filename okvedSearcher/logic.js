4415d8d25085ac5bb330ebd6600d9376c08e1aa1
GuardCat <storktec@mail.ru>
2020-06-05 12:49:22 +0300

    Код: разработаны функции нормализации слов, сортировки при поиске. База: добвлены расшифровки кодов на 95. Добавлены ключеые слова Ремонт квартир в базу ОКВЭД

--- okvedSearcher/logic.js
+++ okvedSearcher/logic.js
@@ -1,20 +1,34 @@
-/*jshint esversion: 8, browser: true*/
-
+/*jshint esversion: 9, browser: true*/
 
 async function director( ) {
-	const 
-		okveds = await getJson("okved.json?1"),
+	const
+		okveds = await getJson("okved.json"),
 		areas =  await getJson("area.json"),
 		main = document.body.querySelector("main"),
-		testArr = grep(okveds, ["code"], ["95"])  
+		keys = ["code", "name", "desc"],
+		input = document.querySelector("input[name='mainField']"),
+		searchWhenChanged = doSearch.bind(null, input, okveds, areas, keys, main)
+		//,testArr = grepMissedCodes(okveds, areas)
+		//,testArr = grep(okveds, ["name"], ["изготов"])
 	;
-	
-	addRow(testArr, areas, main);
+
+	input.addEventListener("keyup", searchWhenChanged, false);
+	//addRow(testArr, areas, main, false);
+	//console.log(testArr.length);
+}
+
+function doSearch(input, okveds, areas, keys, main) {
+	const
+		text = input.value,
+		words = textToArray(text).filter( word => word.length > 1),
+		found = grep(okveds, keys, words)
+	;
+	main.innerHTML = "";
+	addRow(found, areas, main);
 }
 
 async function getJson(url) {
-	if (!url) throw new Error(`getJson: recieved empty url`);
-	
+
 	return new Promise( async (resolve, reject) => {
 		let responce = await fetch(url);
 		if (!responce.ok) throw new Error(`getJson: Can't recieve the file "${url}". Server response status: ${responce.status}: ${responce.statusText}`);
@@ -22,11 +36,11 @@ async function getJson(url) {
 	} );
 }
 
-function addRow(okvedEntry, areas, block) {
+function addRow(okvedEntry, areas, block, descFlag) {
 	if (okvedEntry instanceof Array) return okvedEntry.map( entry => addRow(entry, areas, block) );
-	
+
 	let areaEntry = areas.filter(el => el.code === okvedEntry.code)[0];
-	if (!areaEntry) areaEntry = areas.filter(el => el.code === "96.03")[0];
+	if (!areaEntry) areaEntry = {area: "Нет данных"};
 	block.insertAdjacentHTML( "beforeend", createO(okvedEntry) );
 	block.insertAdjacentHTML( "beforeend", createA(areaEntry) );
 }
@@ -35,24 +49,40 @@ function createA(entry) {
 	return `<p>${entry.area}</p>`;
 }
 
-function createO(entry) {
-	return `
-		<p>
-			<strong>${entry.code} ${entry.name}</strong>
-			${entry.desc}
-		</p>
-	`.replace(/\^/gi,"<br>");
+function createO(entry, descFlag = true) {
+	const
+		message = `<p><strong>${entry.code} ${entry.name}</strong>`,
+		closer = `</p>`,
+		desc =	`${entry.desc}`.replace(/\^/gi,"<br>")
+	;
+	return message + (descFlag ? desc + closer : closer);
 }
 
 function grep(arr, keys, words) {
-	return words.reduce( 
+	if (!words.length) return [{code:"", name: "введите больше слов", desc: ""}];
+	return words.reduce(
 		(res, word) => {
 			return res.filter( el => {
 				let r = new RegExp(word, "gi");
 				return keys.some( (key) => r.test(el[key]) );
 			} );
-		},
-	arr);
+		}, arr)
+		.sort( (a, b) => {
+			const phrase = new RegExp( words.join(" "), "gi");
+			return keys.some( key => phrase.test(a[key]) ) ? -1 : 0;
+		} );
+}
+
+function grepMissedCodes(okveds, areas) {
+	return okveds.filter( okved => !areas.some( area => area.code === okved.code ) );
+}
+
+function textToArray(text) {
+	const
+		badSymbols = /[^А-ЯЁ0-9\. ]/gi,
+		spaces = / +/g
+	;
+	return text.replace(badSymbols, "").split(spaces).filter( text => !!text );
 }
 
 window.addEventListener("load", director, false);
