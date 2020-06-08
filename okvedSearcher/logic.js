6dddbd01d5a7ad37adcae03cdcb8641ffa6ef193
GuardCat <storktec@mail.ru>
2020-06-08 16:13:11 +0300

    Выделяем найденное. Рефакторинг

--- okvedSearcher/logic.js
+++ okvedSearcher/logic.js
@@ -1,30 +1,35 @@
 /*jshint esversion: 9, browser: true*/
-
+"use strict";
 async function director( ) {
+	let timeId = 0;
 	const
 		okveds = await getJson("okved.json"),
 		areas =  await getJson("area.json"),
 		main = document.body.querySelector("main"),
 		keys = ["code", "name", "desc"],
 		input = document.querySelector("input[name='mainField']"),
-		bindedSearch = doSearch.bind(null, input, okveds, areas, keys, main),
-		searchWhenChanged = ( ) => timeId = setTimeout(bindedSearch, 1000)
-		//,testArr = grepMissedCodes(okveds, areas)
-		//,testArr = grep(okveds, ["name"], ["изготов"])
+		search = (t = 1000) => {
+			clearTimer( );
+			timeId = setTimeout(doSearch, t, input, okveds, areas, keys, main);
+		},
+		searchNotEnter = (e) => {
+			if (e.keyCode !== 13) search( );
+		},
+		clearTimer = ( ) => {
+			if (timeId) clearTimeout(timeId);
+		}
 	;
-	let timeId;
 
-	input.addEventListener("keyup", searchWhenChanged, false);
-	input.addEventListener( "keydown", ( ) => clearTimeout(timeId) );
-	//addRow(testArr, areas, main, false);
-	//console.log(testArr.length);
+	input.addEventListener("keyup", searchNotEnter, false);
+	input.addEventListener("keydown", clearTimer, false);
+	document.forms.mainForm.addEventListener( "submit", (e) => {
+		e.preventDefault( );
+		search(10);
+	} );
 }
 
 function doSearch(input, okveds, areas, keys, main) {
-	const
-		text = input.value,
-		words = textToArray(text).filter( word => word.length > 1)
-	;
+	const words = textToArray(input.value).filter( word => word.length > 1);
 	main.innerHTML = "<p>Поиск...</p>";
 	setTimeout( ( ) => {
 		let found = grep(okveds, keys, words);
@@ -43,15 +48,21 @@ async function getJson(url) {
 }
 
 function addRow(okvedEntry, areas, block, descFlag) {
-	if (okvedEntry instanceof Array) return okvedEntry.map( entry => addRow(entry, areas, block) );
-
-	let areaEntry = areas.filter(el => el.code === okvedEntry.code)[0];
-	if (!areaEntry) return false; //areaEntry = {area: "Нет данных"};
+	if (okvedEntry instanceof Array) {
+		if (okvedEntry.length > 1) {
+			block.insertAdjacentHTML( "beforeend", createO({desc: `Найдено вариантов: ${okvedEntry.length}`,code: "", name: ""}) );
+			block.insertAdjacentHTML( "beforeend", createA({area: ""}) );
+		}
+		return okvedEntry.map( entry => addRow(entry, areas, block) );
+	}
+	let areaEntry = areas.filter(area => area.code === okvedEntry.code.replace(/<[^>]+>/g, ""))[0]; // При сравнении убираем span — выделение найденного
+	if (!areaEntry && okvedEntry.code) return false; //areaEntry = {area: "Нет данных"};
+	if (!okvedEntry.code) areaEntry = {area: ""};
 	block.insertAdjacentHTML( "beforeend", createO(okvedEntry) );
 	block.insertAdjacentHTML( "beforeend", createA(areaEntry) );
 }
 
-function createA(entry) {			
+function createA(entry) {
 	return `<p>${entry.area}</p>`;
 }
 
@@ -65,7 +76,7 @@ function createO(entry, descFlag = true) {
 }
 
 function grep(arr, keys, words) {
-	if (!words.length) return [{code:"", name: "введите больше слов", desc: ""}];
+	if (!words.length) return [{code:"", name: "введите больше слов (латиница и символы не учитываются)", desc: ""}];
 	const result = words.reduce(
 		(res, word) => {
 			return res.filter( el => {
@@ -74,15 +85,38 @@ function grep(arr, keys, words) {
 			} );
 		}, arr)
 		.sort( (a, b) => {
-			const phrase = new RegExp( words.join(" "), "gi");
-			let result = 0;
-			if ( phrase.test(a.name) ) return -1;
-			if ( keys.some( key => phrase.test(a[key]) ) ) return -1;
+			const
+				phrase = new RegExp( words.join(" "), "gi"),
+				startPhrase = new RegExp( "^" + words.join(" "), "gi")
+			;
+			if ( startPhrase.test(a.code) ) return -1;
+
+			if ( startPhrase.test(b.code) ) return 0;
+			if ( phrase.test(a.code) ) return -1;
+
+			if ( phrase.test(b.code) ) return 0;
+			if ( phrase.test(a.name)  ) return -1;
+
+			if ( phrase.test(b.name) ) return 0;
+			if ( phrase.test(a.desc)  ) return -1;
+
+			if ( phrase.test(b.desc)  ) return 0;
 			if ( words.some( (word) => a.name.search(word) !== -1 ) ) return -1;
 			return 0;
+	} ).map( element => {
+		const mapResult = { };
+		keys.forEach( key => {
+			words.forEach( word => {
+				const r = new RegExp(`(${word})`, "gi");
+				mapResult[key] = element[key].replace(r, "<span class='found'>$1</span>");
+			} );
+		} );
+		return mapResult;
 	} );
 
-	if (!result.length) return [{code:"", name: "Ничего не найдено. Попробуйте сформулировать иначе.", desc: ""}];
+	if (!result.length) {
+		return [{code:"", desc: "Ничего не найдено. Попробуйте сформулировать иначе.", name: ""}];
+	}
 	return result;
 }
 
