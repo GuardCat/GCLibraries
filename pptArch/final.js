ada49115a23b37b0bb56fd1b0d8aefb57e125031
GuardCat <storktec@mail.ru>
2020-06-26 15:38:01 +0300

    js для сжатия картинок през node

---
+++ pptArch/final.js
@@ -0,0 +1,18 @@
+const
+	fs = require("fs"),
+	{exec} = require("child_process"),
+;
+
+let list, arr;
+
+exec(`mogrify -colors 256 ./low/*`);
+exec(`mogrify -colors 512 *.png *.jpg *.jpeg`);
+exec(`mv ./low/* ./`);
+exec(`mv ./high/* ./`);
+setTimeout(final, 1000);
+
+
+function final( ) {
+	exec ("optipng *.png");
+	exec ("jpegoptim *.jpg *.jpeg");
+}
---
+++ pptArch/first.js
@@ -0,0 +1,33 @@
+const
+	fs = require("fs"),
+	{exec} = require("child_process"),
+	minOption = "-25k",
+	maxSize = 1200
+;
+
+let list, arr;
+exec("rm *.svg *.wdp", options);
+exec("identify *.png *.jpg *.jpeg | cut -f 1 -d 'x' > list.txt");
+
+setTimeout(resize, 1000);
+
+function resize( ) {
+	list = fs.readFileSync("./list.txt", "utf8");
+	arr = list.split("\n");
+
+	arr = arr.filter( e => {
+		size = +e.split(/ +/g)[2];
+		return size > maxSize;
+	} );
+
+	arr.forEach( e => {
+		name = e.split(/ +/g)[0];
+		exec(`mogrify -resize ${maxSize} ${name}`)
+	} );
+	
+	exec("identify *.png *.jpg *.jpeg | grep -i grayscale | cut -f 1 -d ' ' | xargs -I {} rm {}");
+	exec(`find . -iname '*.png' -size ${minOption} -delete`);
+	exec(`find . -iname '*.jpg' -size ${minOption} -delete`);
+	exec(`find . -iname '*.jpeg' -size ${minOption} -delete`);	
+
+}
