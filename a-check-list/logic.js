aa9696257c17f63e47473ecd9c6a335e71cea5c8
Nikolay <writeto@bk.ru>
2021-10-18 17:13:17 +0300

    [CHECKLIST] x_по Согласию и ПВ

--- a-check-list/logic.js
+++ a-check-list/logic.js
@@ -88,8 +88,8 @@ class TieChecker {
 
 function director( ) {
 	const
-		version = "v1.4",
-		updatedFact = 1634293507406,
+		version = "v1.4.1",
+		updatedFact = 1634556091192,
 		allowedUpdateDate = 1634293507406,
 		updatedLocal = +window.localStorage.getItem("checklist_auto_updated"),
 		oldDate = new Date(updatedLocal),
