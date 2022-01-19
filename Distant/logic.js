2f93d7f256679fb5a7b7dbc4b4bee53a84c73729
Nikolay <writeto@bk.ru>
2022-01-19 18:05:55 +0300

    [DISTANT]  - Убрана ручная логика проверки, кроме LastSym  + Добавлена логика на основе системной валидации

--- Distant/logic.js
+++ Distant/logic.js
@@ -1,91 +1,43 @@
 
 class Validator {
-	constructor({input, regs, minLen = 0, maxLen = +Infinity, startValue, buttons = []}) {
+	constructor({input, regs, startValue}) {
 		this.input = input;
 		this.regs = regs;
-		this.minLen = minLen;
-		this.maxLen = maxLen;
 		this.oldValue = startValue;
-		this.buttons = buttons;
 		if (startValue) input.value = startValue;
-		this.setState( );
 
 		input.addEventListener("keyup", this.validateLastSym.bind(this), false);
-		input.addEventListener("blur", this.setClass.bind(this), false);
-		input.addEventListener("change", this.setClass.bind(this), false);
 	}
 
 	validateLastSym( ) {
 		if ( this.regs.some( reg => reg.test(this.input.value) ) || this.input.value === "" ) {
 			this.oldValue = this.input.value;
-			this.input.classList.contains("wrong") ? this.setClass( ) :	this.setState( );
-			return true;
-		}
-		this.input.value = this.oldValue;
-		this.setState( );
-		return false;
-	}
-
-	setClass( ) {
-		const changed = this.setState( ) === this.input.classList.contains("wrong");
-		if(this._good) { 
-			this.input.classList.remove("wrong")
+			localStorage.setItem( this.input.id, this.input.value.toUpperCase( ) );
 		} else {
-			this.input.classList.add("wrong");
+			this.input.value = this.oldValue;
 		}
-		if (changed) document.dispatchEvent( new CustomEvent("stateChanged") );
-		return this._good;
 	}
-
-	setState( ) {
-		const state = ( this.regs[0].test(this.input.value) && this.input.value.length >= this.minLen && this.input.value.length <= this.maxLen );
-		this._good = state;
-		return state;
-	}
-
-	get good( ) {
-		if (this._good) localStorage.setItem(this.input.id, this.input.value)
-		return this._good
-	}
-
 }
 
 function direktor( ) {
 	const 
 		go = document.querySelector("#go"),
 		login = document.querySelector("#login"),
+		form = document.querySelector("form"),
 		tt = document.querySelector("#tt"),
 		copyButton = document.querySelector("#copy"),
 		buttons = [copyButton, go],
 		ttRegs = [/^\d{10,16}$/, /^\d+$/],
 		loginRegs = [ 
-			/^W\d+N{0,1}\d+$/i,
-			/^W\d+N$/i,
-			/^W\d+$/i,
+			/^W\d+N{0,1}\d{0,}$/i,
 			/^W\d{0,}$/i
-		],
-		indicators = [
-			new Validator( {input: login, regs: loginRegs, minLen: 4, maxLen: login.getAttribute("maxlength"), startValue: localStorage.getItem("login")} ),
-			new Validator( {input: tt, regs: ttRegs, startValue: localStorage.getItem("tt"), maxLen: tt.getAttribute("maxlength"), minLen: 10} )
-		]
-	;
+		];
+	new Validator( {input: login, regs: loginRegs, startValue: localStorage.getItem("login")} );
+	new Validator( {input: tt, regs: ttRegs, startValue: localStorage.getItem("tt")} );
 		
-	login.addEventListener("keyup", raiseLetters, false);
-
-	document.addEventListener("stateChanged", ( ) => setButtonsState( indicators.every(i => i.good), buttons ), false);
-
-	go.addEventListener("click", ( ) => {
-		const state = indicators.every(i => i.good);
-		setButtonsState(state, buttons);
-		if (state) openLink(login, tt);
-	}, false);
-
-	copyButton.addEventListener("click", ( ) => {
-		const state = indicators.every(i => i.good);
-		setButtonsState(state, buttons);
-		if (state) copyLink(login, tt);
-	}, false);
-
+	login.addEventListener("change", raiseLetters, false);
+	document.addEventListener( "keyup", ( ) => setButtonsState( form.checkValidity( ), buttons ) );
+	copyButton.addEventListener("click", ( ) => copyLink(login, tt), false);
 }
 
 function setButtonsState(state, buttons) {
@@ -99,14 +51,8 @@ function setButtonsState(state, buttons) {
 }
 
 function raiseLetters(e) {
-	e.target.value = e.target.value.toUpperCase( );
+	if(e.target.validity.valid) e.target.value = e.target.value.toUpperCase( );
 }
-
-function openLink(login, tt) {
-	saveToLS(login, tt);
-	document.location.href = generateLink(login, tt);
-}
-
 
 function copyLink(login, tt) {
 	const url = generateLink(login, tt);
@@ -114,12 +60,7 @@ function copyLink(login, tt) {
 }
 
 function generateLink(login, tt) {
-	return `https://cash.otpbank.ru/?utm_source=MMO&utm_medium=${tt.value}&utm_campaign=${login.value}`;
-}
-
-function saveToLS(login, tt) {
-	localStorage.setItem("tt", tt.value);
-	localStorage.setItem("login", login.value);
+	return `https://cash.otpbank.ru/?utm_source=MMO&${tt.name}=${tt.value}&${login.name}=${login.value}`;
 }
 
 direktor( );
