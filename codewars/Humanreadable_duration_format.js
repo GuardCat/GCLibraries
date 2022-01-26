1e703b758a7fd30fe6d496476097a7a16b800ce9
Nikolay <writeto@bk.ru>
2022-01-26 12:16:25 +0300

    [CODEWARS] * HR duration is finalized

--- codewars/Humanreadable_duration_format.js
+++ codewars/Humanreadable_duration_format.js
@@ -1,18 +1,27 @@
 // Human readable duration format
 // https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/javascript
+//Done
 
 function formatDuration (seconds) {
-	let 
-		minutes = Math.floor(seconds / 60),
-		hours = Math.floor(minutes / 60),
-		delimiters = ["", ", ", " and "],
-		result = ""
-	;
-	minutes -= hours * 60;
-	seconds -= hours * 3600 + minutes * 60;
+	const periods = [
+		{name: "second", val: 1},
+		{name: "minute", val: 60},
+		{name: "hour", val: 60 * 60},
+		{name: "day", val: 60 * 60 * 24},
+		{name: "year", val: 60 *60 * 24 * 365}
+	];
+	if (seconds === 0) return "now";
+	let result = periods.reduceRight( (res, period) => {
+		if (period.val > seconds) return res;
+		const
+			periodRes = Math.floor(seconds / period.val),
+			name = periodRes > 1 ? period.name + "s" : period.name
+		;
+		seconds = seconds % period.val;
 
-	result += hours ? `${hours} hour${hours > 1 ? "s" : ""}` : "";
-	result += minutes ? `${result.length ? "," : ""} ${minutes} minute${minutes > 1 ? "s" : ""}` : "";
-	result += seconds ? `${result.length ? "," : ""} ${seconds} second${seconds > 1 ? "s" : ""}` : "";
+		if (res.length) res += ", ";
+		return res + `${periodRes} ${name}`
+	}, "");
+
 	return result.replace(/\, ([^,]+)$/, " and $1");
 }
