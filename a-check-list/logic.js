fdce4fce0adee0c22e6fe3577f175637b99496b7
Nikolay <writeto@bk.ru>
2022-03-23 17:45:49 +0300

    [ЧЕКЛИСТ] * уточнили данные по проверке залогов

--- a-check-list/logic.js
+++ a-check-list/logic.js
@@ -92,9 +92,9 @@ class TieChecker {
 
 function director( ) {
 	const
-		version = "v1.5.1",
-		updatedFact = 1647434673008,
-		allowedUpdateDate = 1647434673008,
+		version = "v1.5.2",
+		updatedFact = 1648046693114,
+		allowedUpdateDate = 1648046693114,
 		updatedLocal = +window.localStorage.getItem("checklist_auto_updated"),
 		oldDate = new Date(updatedLocal),
 		newDate = new Date(updatedFact),
@@ -117,9 +117,9 @@ function director( ) {
 `Приложение обновлено.
 Данные старой версии ${ updatedLocal > 0 ? "от " + zeroTo( oldDate.getDate( ) ) + "." + zeroTo( oldDate.getMonth( ) + 1 ) + "." + oldDate.getFullYear( ) : ""} удалены, чек-лист будет очищен.
 Изменения:
+v1.5.2: уточнены условия проверки на fssp и в реестре залогов.
 v1.5.1: полностью обновлены документы и требования к письмам.
 v1.4: + детализированы требования РГО. x Исправлены ошибки (остатки ГП)
-v1.31: - удалена ГП
 `,
 		renewAll = ( ) => {
 			let numOffVidgets, checklistVisible;
