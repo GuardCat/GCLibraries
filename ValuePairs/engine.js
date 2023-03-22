6ee2e2ccf45ff42b0d039774103b28b053776918
GuardCat <storktec@mail.ru>
2023-03-22 18:17:26 +0300

    [VP] * вот теперь работает и оформлено

--- ValuePairs/engine.js
+++ ValuePairs/engine.js
@@ -4,22 +4,12 @@ const
 	addButton = document.getElementById("addValButton"),
 	monitor = document.getElementById("monitor").querySelector("tbody"),
 	recalcButton =  document.getElementById("recalc"),
-	reset =  document.getElementById("reset"),
+	resetButton =  document.getElementById("reset"),
 	fPairButton = document.getElementById("FirstOfThePair"),
 	sPairButton = document.getElementById("SecondOfThePair"),
 	recalcGUI =  document.getElementById("recalcGUI"),
 	regularGUI =  document.getElementById("regularGUI")
 ;
-
-async function pairize(arr, fn) {
-	arr.forEach( (first, i, arr) => {
-		slice = arr.slice(i + 1);
-			slice.forEach( async second => {
-				await fn(first, second)
-				console.log(first);
-		});
-	} );
-}
 
 document.getElementById("enterIt").onsubmit = (e) => {
 	e.preventDefault( );
@@ -27,17 +17,17 @@ document.getElementById("enterIt").onsubmit = (e) => {
 }
 
 class Base {
-	constructor ( { vField, addButton, monitor, recalcButton, lsKey, pairize, fPairButton, sPairButton, recalcGUI, regularGUI }) {
+	constructor ( { vField, addButton, monitor, recalcButton, lsKey, fPairButton, sPairButton, recalcGUI, regularGUI, resetButton}) {
 		this.vField = vField;
 		this.addButton = addButton;
 		this.monitor = monitor;
 		this.recalcButton = recalcButton;
 		this.lsKey = lsKey;
-		this.pairize = pairize;
 		this.sPairButton = sPairButton;
 		this.fPairButton = fPairButton;
 		this.recalcGUI = recalcGUI;
 		this.regularGUI = regularGUI;
+		this.resetButton = resetButton
 		
 		this.readBase( );
 	
@@ -49,18 +39,42 @@ class Base {
 			this.addItem(this.vField.value);
 		} );
 
-		recalcButton.addEventListener( "click", async ( ) => {
+		resetButton.addEventListener("click", ( ) => this.resetSums( ) );
+
+		recalcButton.addEventListener( "click", ( ) => {
 			this.regularGUI.classList.add("hidden");
 			this.recalcGUI.classList.remove("hidden");
-			await this.pairize(this.base, this.recalcSums.bind(this) );
-			this.regularGUI.classList.remove("hidden");
-			this.recalcGUI.classList.add("hidden");
-			this.saveBase( );
+			this.recalc( this[Symbol.iterator]( ) );
 		} );
 
 	}
 
+	recalc(iterator) {
+		let position = iterator.next( );
+		if (position.done) {
+			this.regularGUI.classList.remove("hidden");
+			this.recalcGUI.classList.add("hidden");
+			this.sort( );
+			this.saveBase( );
+			return true;
+		}
+
+		this.fPairButton.value = this.base[position.value.fpos].desc;
+		this.fPairButton.onclick = ( ) => {
+			this.addSum(position.value.fpos);
+			this.recalc(iterator);
+		}
+
+		this.sPairButton.value = this.base[position.value.spos].desc;
+		this.sPairButton.onclick = ( ) => {
+			this.addSum(position.value.spos);
+			this.recalc(iterator);
+		}
+
+	}
+
 	addItem(desc) {
+		if ( !desc || this.base.some( entry => entry.desc === desc ) ) return false;
 		this.base.push( {desc, sum: 0} );
 		this.vField.value = "";
 		this.saveBase( );
@@ -80,30 +94,13 @@ class Base {
 		this.base.forEach( field => field.sum = 0 );
 		this.saveBase( );
 	}
-
-	async recalcSums(first, second) {
-		this.sPairButton.value = first.desc;
-		this.fPairButton.value = second.desc;
-		return new Promise( resolve => {
-			this.sPairButton.onclick = ( ) => {
-				first.sum += 1
-				resolve(true);
-			}
-
-			this.fPairButton.onclick = ( ) => {
-				second.sum += 1
-				resolve(true);
-			}
-		})
-	}
-
 	reloadMonitor( ) {
 		let table = "";
 		this.base.forEach( (field,i) => {
 			table += `<tr>
 							<td>${field.desc}</td>
 							<td>${field.sum}</td>
-							<td><input type='button' onclick='base.delItem(${i})' value="x"></td>
+							<td><input type='button' class='deleter' onclick='base.delItem(${i})' value="X"></td>
 			</tr>`
 		});
 		this.monitor.innerHTML = table;
@@ -112,13 +109,36 @@ class Base {
 	readBase( ) {
 		const rawBase = localStorage.getItem(this.lsKey);
 		this.base = rawBase ? JSON.parse( rawBase ) : [ ];
+		this.sort( );
 		this.reloadMonitor( );
+	}
+
+	sort( ) {
+		this.base.sort( (a, b) => a.sum < b.sum ? 1 : -1 );
 	}
 
 	saveBase( ) {
 		localStorage.setItem( this.lsKey, JSON.stringify(this.base) );
 		this.reloadMonitor( );
 	}
+
+	[Symbol.iterator]( ) {
+		let fpos = 0, spos = 0;
+		const max = this.base.length - 1;
+		return {
+			next( ) {
+				if (spos < max) {
+					spos++;
+				} else {
+					fpos++;
+					if (fpos >= max) return {done: true};
+					spos = fpos + 1
+				}
+				return {done: false, value: {fpos, spos} }
+			}
+		}
+	}
+
 }
 
-let base = new Base( {vField, addButton, monitor, recalcButton, lsKey, pairize, recalcGUI, regularGUI, sPairButton, fPairButton} );
+let base = new Base( {vField, addButton, monitor, recalcButton, lsKey, recalcGUI, regularGUI, sPairButton, fPairButton, resetButton} );
