aea21cb4f8ebda89e22c884c0d691f623b635c5b
GC <guardcat@mail.ru>
2025-02-28 23:19:19 +0300

    [Py bal] добавлена секретная страница

--- python/web/monmon/app.py
+++ python/web/monmon/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import sqlite3
 import random as r
 
@@ -6,18 +6,31 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
-def get_data():
-	res = {}
-	connect = sqlite3.connect("/home/guardcat/dbs/money/money.db")
-	cursor = connect.cursor()
-	cursor.execute("SELECT * from v_by_bud_html")
-	res["data"] = cursor.fetchall()
-	cursor.execute("SELECT MAX(date) from transact")
-	res["date"] = cursor.fetchone()
-	cursor.execute("SELECT name, sum(real_sum) FROM transact INNER JOIN accounts ON account_id = accounts.id WHERE account_id = '5'")
-	res["balance"] = cursor.fetchall()
-	connect.close()
-	return res
+def get_data(view='v_by_bud_html', fetch_all = False):
+    res = {}
+    one_account = """
+        SELECT name, sum(real_sum)\
+        FROM transact
+        INNER JOIN accounts ON account_id = accounts.id
+        WHERE account_id = '5'
+    """
+    all_accouns = """
+        SELECT name, sum(real_sum) 
+        FROM transact 
+        INNER JOIN accounts ON account_id = accounts.id
+        WHERE accounts.active = '1'
+        GROUP BY accounts.id
+    """
+    connect = sqlite3.connect("/home/guardcat/dbs/money/money.db")
+    cursor = connect.cursor()
+    cursor.execute(f"SELECT * from {view}")
+    res["data"] = cursor.fetchall()
+    cursor.execute("SELECT MAX(date) from transact")
+    res["date"] = cursor.fetchone()
+    cursor.execute(all_accounts if fetch_all else one_account)
+    res["balance"] = cursor.fetchall()
+    connect.close()
+    return res
 
 
 def get_fake_data():
@@ -32,31 +45,50 @@ def get_fake_data():
     return res
 
 def format_data(arr, hide_them=True):
-	res = []
-	names = {
-		"Фонд жилья": "Отложено",
-		"Сбер Моментум": "Баланс на сбере"
-	}
-	to_hide = ["Стрижка Эрика"]
-	for el in arr:
-		l_res = []
-		if el[0] in to_hide and hide_them:
-			continue
-		if el[0] in names:
-			l_res.append(names[el[0]])
-		else:
-			l_res.append(el[0])
-		l_res.append(f"{el[1]:_.0f}".replace("_", " "))
-		res.append(l_res)
-	
-	return res
+    res = []
+    names = {
+        "Фонд жилья": "Отложено",
+        "Сбер Моментум": "Баланс на сбере"
+    }
+    to_hide = ["Стрижка Эрика"]
+    for el in arr:
+        l_res = []
+        if el[0] in to_hide and hide_them:
+            continue
+        if el[0] in names:
+            l_res.append(names[el[0]])
+        else:
+            l_res.append(el[0])
+        l_res.append(f"{el[1]:_.0f}".replace("_", " ").replace("-0", "0"))
+        res.append(l_res)
+    
+    return res
+
+@app.route('/fake_data')
+def fake():
+    arr = get_fake_data()
+    arr["data"] = format_data(arr["data"])
+    arr["balance"] = format_data(arr["balance"])
+    date = ".".join(reversed(arr["date"][0].split("-")[1:]))
+    return render_template("index.html", data=arr["data"], date=date, balance=arr["balance"])
 
 @app.route('/Hdhfbmbkhuk83yhsjdsbfmnb--sdLLlksdjsdjkfjkfdsjljdl')
 def minmon():
-	arr = get_data()
-	arr["data"] = format_data(arr["data"])
-	arr["balance"] = format_data(arr["balance"])
-	date = ".".join(reversed(arr["date"][0].split("-")[1:]))
-	return render_template("index.html", data=arr["data"], date=date, balance=arr["balance"])
+    arr = get_data()
+    arr["data"] = format_data(arr["data"])
+    arr["balance"] = format_data(arr["balance"])
+    date = ".".join(reversed(arr["date"][0].split("-")[1:]))
+    return render_template("index.html", data=arr["data"], date=date, balance=arr["balance"])
 
+
+@app.route('/gdjfovbnmHgd-HCTpvdbute-sjnnn628awfgfsa')
+def maxmon():
+    arr = get_data(view="v_by_bud_html_all")
+    arr["data"] = format_data(arr["data"], False)
+    arr["balance"] = format_data(arr["balance"], False)
+    date = ".".join(reversed(arr["date"][0].split("-")[1:]))
+    return render_template("index.html", data=arr["data"], date=date, balance=arr["balance"])
+
+
+    
 app.run(port="8080", host="0.0.0.0")
